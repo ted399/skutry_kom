399c163bdcb22a8bd24690e56dd001b05a33da7d
Tadeas Hort <tadeashort@centrum.cz>
2018-12-03 21:27:33 +0100

    add req for scraper

--- scraper.js
+++ scraper.js
@@ -1,93 +1,95 @@
-const algoliasearch = require('algoliasearch');
-const dotenv = require('dotenv');
-const firebase = require('firebase');
+function scrape(){
+  const algoliasearch = require('algoliasearch');
+  const dotenv = require('dotenv');
+  const firebase = require('firebase');
 
-// load values from the .env file in this directory into process.env
-dotenv.load();
+  // load values from the .env file in this directory into process.env
+  dotenv.load();
 
-// configure firebase
-firebase.initializeApp({
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
-const database = firebase.database();
+  // configure firebase
+  firebase.initializeApp({
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+  });
+  const database = firebase.database();
 
-// configure algolia
-const algolia = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY
-);
-const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
+  // configure algolia
+  const algolia = algoliasearch(
+    process.env.ALGOLIA_APP_ID,
+    process.env.ALGOLIA_API_KEY
+  );
+  const index = algolia.initIndex(process.env.ALGOLIA_INDEX_NAME);
 
 
-    //add both Scrapers
-    const scraperBazos = require('./scraperBazos.js');
-    const scraperSbazar = require('./scraperSbazar.js');
+      //add both Scrapers
+      const scraperBazos = require('./scraperBazos.js');
+      const scraperSbazar = require('./scraperSbazar.js');
 
-    (async () => {
-      //Run both scrapers
-      const offersBazos = await scraperBazos;
-      const offersSbazar = await scraperSbazar;
+      (async () => {
+        //Run both scrapers
+        const offersBazos = await scraperBazos;
+        const offersSbazar = await scraperSbazar;
 
-      //Combine scrape data from both scrapers into one array of objects
-      //var offers = offersBazos.concat(offersSbazar);
-      function m(a, b) {
-        const l = Math.max(a.length, b.length);
-        const result = [];
-        for (let i = 0; i < l; i++) {
-          if (a[i] !== undefined) {
-            result.push(a[i]);
+        //Combine scrape data from both scrapers into one array of objects
+        //var offers = offersBazos.concat(offersSbazar);
+        function m(a, b) {
+          const l = Math.max(a.length, b.length);
+          const result = [];
+          for (let i = 0; i < l; i++) {
+            if (a[i] !== undefined) {
+              result.push(a[i]);
+            }
+            if (b[i] !== undefined) {
+              result.push(b[i]);
+            }
           }
-          if (b[i] !== undefined) {
-            result.push(b[i]);
-          }
+
+          //console.log(result);
+          return result;
+        };
+
+        const offers = m(offersBazos, offersSbazar)
+
+        //Start process of pushing the whole array of objects into firebae and then into algolia
+
+        //Remove all data from firebase ref
+        database.ref('/skutry_novy').remove();
+        //Push newly scraped data into firebase
+        for(i=offers.length; i >= 0; i--) {
+            database.ref('/skutry_novy').push(offers[i]);
         }
 
-        //console.log(result);
-        return result;
-      };
+        //Algolia
+        //Delete all the data from an Index
+        await index.clearIndex()
 
-      const offers = m(offersBazos, offersSbazar)
+        // Get all contacts from Firebase
+        database.ref('/skutry_novy').once('value', contacts => {
+          // Build an array of all records to push to Algolia
+          const records = [];
+          contacts.forEach(contact => {
+            // get the key and data from the snapshot
+            const childKey = contact.key;
+            const childData = contact.val();
+            // We set the Algolia objectID as the Firebase .key
+            childData.objectID = childKey;
+            // Add object for indexing
+            records.push(childData);
+          });
 
-      //Start process of pushing the whole array of objects into firebae and then into algolia
 
-      //Remove all data from firebase ref
-      database.ref('/skutry_novy').remove();
-      //Push newly scraped data into firebase
-      for(i=offers.length; i >= 0; i--) {
-          database.ref('/skutry_novy').push(offers[i]);
-      }
 
-      //Algolia
-      //Delete all the data from an Index
-      await index.clearIndex()
-
-      // Get all contacts from Firebase
-      database.ref('/skutry_novy').once('value', contacts => {
-        // Build an array of all records to push to Algolia
-        const records = [];
-        contacts.forEach(contact => {
-          // get the key and data from the snapshot
-          const childKey = contact.key;
-          const childData = contact.val();
-          // We set the Algolia objectID as the Firebase .key
-          childData.objectID = childKey;
-          // Add object for indexing
-          records.push(childData);
+          // Add or update new objects
+          index
+            .saveObjects(records)
+            .then(() => {
+              console.log('Contacts imported into Algolia');
+              process.exit();
+            })
+            .catch(error => {
+              console.error('Error when importing contact into Algolia', error);
+              process.exit(1);
+            });
         });
 
-
-
-        // Add or update new objects
-        index
-          .saveObjects(records)
-          .then(() => {
-            console.log('Contacts imported into Algolia');
-            process.exit();
-          })
-          .catch(error => {
-            console.error('Error when importing contact into Algolia', error);
-            process.exit(1);
-          });
-      });
-
-    })();
+      })();
+};//End of Scrape()
--- server.js
+++ server.js
@@ -2,9 +2,14 @@ const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
 const path = require('path');
+const scraper = require('./scraper.js');
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/scrape', function(req, res) {
+    scrape();
 });
 
 app.use(express.static('public'));
